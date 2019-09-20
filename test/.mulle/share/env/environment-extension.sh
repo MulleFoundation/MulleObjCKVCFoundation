bc246082b950f25fca9ff53d76dafcec9494eb42
Nat! <nat@codeon.de>
2019-09-20 17:20:38 +0200

    update mulle-sde

---
+++ test/.mulle/share/env/environment-extension.sh
@@ -0,0 +1,12 @@
+# These are used by mulle-match find to speed up the search.
+export MULLE_MATCH_FIND_NAMES="*.stdout,*.stdin,*.stderr,*.errors,*.ccdiag"
+
+
+# Make project to test discoverable via MULLE_FETCH_SEARCH_PATHnWe assume we are in ${PROJECT_DIR}/test so modify search path to add ..
+export MULLE_FETCH_SEARCH_PATH="${MULLE_FETCH_SEARCH_PATH}/.."
+
+
+# For more aggressive leak testing, it is good if singletons cleanupnin the NSAutoreleasePool. These ephemerals aren't thread safe singletons.
+export MULLE_OBJC_EPHEMERAL_SINGLETON="YES"
+
+
