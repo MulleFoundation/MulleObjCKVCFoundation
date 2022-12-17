8ffca1a7d0afa133d6d3523ec08bbebbfd607497
Nat! <nat@mulle-kybernetik.com>
2022-12-17 01:19:07 +0100

    up da version

--- src/MulleObjCKVCFoundation.h
+++ src/MulleObjCKVCFoundation.h
@@ -35,7 +35,7 @@
 //
 #import "import.h"
 
-#define MULLE_OBJC_KVC_FOUNDATION_VERSION   ((0 << 20) | (20 << 8) | 1)
+#define MULLE_OBJC_KVC_FOUNDATION_VERSION   ((0 << 20) | (20 << 8) | 2)
 
 #import "import.h"
 
